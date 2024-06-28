125174bc702cafd70a1955696321438c7ba8d2ad
Bongaa <bonga229@gmail.com>
2024-06-28 12:49:08 +0200

    Additional event on key pickup; terrain and pickups flow adjustments

--- Level-Design-Studia/Assets/_Scripts/KeyEvent/KeyEvent.cs
+++ Level-Design-Studia/Assets/_Scripts/KeyEvent/KeyEvent.cs
@@ -4,12 +4,15 @@ public class KeyEvent : MonoBehaviour
 {
     [SerializeField] private GameObject _holeFillObject;
     [SerializeField] private GameObject _eventActionPrefab;
+    [SerializeField] private Animator _gridAnimator;
 
 
     public void TriggerKeyEvent()
     {
         _holeFillObject.SetActive(false);
-        
+
+        _gridAnimator.SetBool("IsKeyPickedUp", true);
+
         Instantiate(_eventActionPrefab);
     }
 }
